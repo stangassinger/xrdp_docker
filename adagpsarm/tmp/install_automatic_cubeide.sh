53ac16d83e8fef21c0c8437305a271067feabd42
Your Name <you@example.com>
2020-01-20 21:01:59 +0100

    adding automatic install

---
+++ adagpsarm/tmp/install_automatic_cubeide.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/expect -f
+log_file -a "expect.log"
+set exp_internal 1 
+set done  0
+set timeout 20
+spawn ./st-stm32cubeide_1.2.0_5034_20200108_0926_amd64.deb_bundle.sh --confirm
+expect  "*Y/n*"
+send    "y\r"
+expect  "*Y/n*"
+send    "y\r"
+expect  "*Y/n*"
+send    "y\r"
+while {$done == 0} {
+sleep 1
+expect {
+"*Moreii*" { send  "\r" }
+"*N/y*"  { send  "y\r"; set done 1 }
+}
+}
