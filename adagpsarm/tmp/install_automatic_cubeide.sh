2203266d0213a05dbcace2ff8e3d7b641ccd1d9f
Your Name <you@example.com>
2020-04-13 18:58:13 +0200

    i...

--- adagpsarm/tmp/install_automatic_cubeide.sh
+++ adagpsarm/tmp/install_automatic_cubeide.sh
@@ -2,8 +2,8 @@
 #log_file -a "expect.log"
 set exp_internal 1 
 set done  0
-set timeout 200
-spawn ./st-stm32cubeide_1.2.0_5034_20200108_0926_amd64.deb_bundle.sh --confirm
+set timeout 200 
+spawn ./st-stm32cubeide_1.3.0_5720_20200220_1053_amd64.deb_bundle.sh --confirm
 expect  "*Y/n*"
 send    "y\r"
 expect  "*Y/n*"
