1c52f043f92d27b84ab867e38d1b6522afd4a440
guenther <guenther.stangassinger@gmx.de>
2020-04-15 21:55:18 +0200

    bugfix

--- adagpsarm/tmp/install_automatic_cubeide.sh
+++ adagpsarm/tmp/install_automatic_cubeide.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/expect -f
 #log_file -a "expect.log"
 set exp_internal 1 
-set done  0
+set done1  0
+set done2  0
 set timeout 200 
 spawn ./st-stm32cubeide_1.3.0_5720_20200220_1053_amd64.deb_bundle.sh --confirm
 expect  "*Y/n*"
@@ -10,11 +11,18 @@ expect  "*Y/n*"
 send    "y\r"
 expect  "*Y/n*"
 send    "y\r"
-while {$done == 0} {
+while {$done1 == 0} {
 sleep 1
 expect {
 "*More*" { send  "\r" }
-"*N/y*"  { send  "y\r"; set done 1 }
+"*N/y*"  { send  "y\r"; set done1 1 }
+}
+}
+while {$done2 == 0} {
+sleep 1
+expect {
+"*More*" { send  "\r" }
+"*N/y*"  { send  "y\r"; set done2 1 }
 }
 }
 expect eof
