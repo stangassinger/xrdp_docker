a640227d604526f0bb95a1c877a3ebefb3cd6e1e
guenther <guenther.stangassinger@gmx.de>
2020-08-28 08:48:40 +0200

    switch to tooling01

--- NN/tmp/install_automatic_cubeide.sh
+++ NN/tmp/install_automatic_cubeide.sh
@@ -4,7 +4,7 @@ set exp_internal 1
 set done1  0
 set done2  0
 set timeout 200 
-spawn ./st-stm32cubeide_1.3.0_5720_20200220_1053_amd64.deb_bundle.sh --confirm
+spawn ./st-stm32cubeide_1.4.0_7511_20200720_0928_amd64.deb_bundle_sh --confirm
 expect  "*Y/n*"
 send    "y\r"
 expect  "*Y/n*"
