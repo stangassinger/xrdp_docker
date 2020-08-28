a8e4ec1a4d1edd02e14f59214ebf7061fa28c9c4
guenther <guenther.stangassinger@gmx.de>
2020-08-28 18:10:57 +0200

    :-)

--- NN/tmp/install_automatic_cubeide.sh
+++ NN/tmp/install_automatic_cubeide.sh
@@ -4,7 +4,7 @@ set exp_internal 1
 set done1  0
 set done2  0
 set timeout 200 
-spawn ./st-stm32cubeide_1.4.0_7511_20200720_0928_amd64.deb_bundle_sh --confirm
+spawn ./st-stm32cubeide_1.4.0_7511_20200720_0928_amd64.deb_bundle.sh --confirm
 expect  "*Y/n*"
 send    "y\r"
 expect  "*Y/n*"
@@ -18,6 +18,9 @@ expect {
 "*N/y*"  { send  "y\r"; set done1 1 }
 }
 }
+#Asking for Segger
+expect  "*Y/n*"
+send    "y\r"
 while {$done2 == 0} {
 sleep 1
 expect {
