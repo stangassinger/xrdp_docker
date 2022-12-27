26f75e2202a01f27fa40254da378fb4ecdb5e4c8
Your Name <you@example.com>
2022-12-27 17:23:46 +0100

    .

--- xrdp3/restoreXfce.sh
+++ xrdp3/restoreXfce.sh
@@ -1,5 +1,19 @@
 ## restore Xfce und Desktop settings from NN
 sudo rm -rf Desktop .config
 sudo cp -r NN/.ssh  NN/.config  NN/Desktop  .
+# needed for frama-c
+sudo cp -r NN/.opam  NN/.why3.conf  . 
 sudo chmod -R 777  .ssh .config     Desktop
 sudo pkill -KILL -u abc
+## add to docker desktop
+#  
+#  [Desktop Entry]
+#  Version=1.0
+#  Type=Application
+#  Name=persistConfig
+#  Comment=
+#  Exec=sh -c "echo 'storing .config'; cd ~; rm -rf NN/.config NN/Desktop; cp -r .config/ NN/.config; cp -r Desktop NN/Desktop; echo 'DONE.' ; sleep 5 "
+#  Icon=checkbox
+#  Path=
+#  Terminal=true
+#  StartupNotify=true
