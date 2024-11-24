0d97a86914bf2f5f77ea452657e4e98c26c9938b
Your Name <you@example.com>
2024-11-24 16:34:53 +0100

    using ubuntu 24.04 instead of 22.04; adding bash-completion

--- xrdp3/restoreXfce.sh
+++ xrdp3/restoreXfce.sh
@@ -4,6 +4,8 @@ sudo cp -r NN/.ssh  NN/.config  NN/Desktop  .
 # needed for frama-c
 sudo cp -r NN/.opam  NN/.why3.conf  . 
 sudo chmod -R 777  .ssh .config     Desktop
+sudo chown abc -R .config
+sudo chgrp abc -R .config
 sudo pkill -KILL -u abc
 ## add to docker desktop
 #  
